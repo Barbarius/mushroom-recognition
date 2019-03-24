b5fe978c62b122d78646ab195c89779e4471b523
Barbarius <s.bysov@mail.ru>
2019-03-24 22:16:36 +0300

    Create first model

---
+++ Model.py
@@ -0,0 +1,44 @@
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+# ResNet model for classifications mushrooms into 3 categories:
+# Edible, non-edible, not-a-mushroom
+model = ResNet50(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=2)
+
+for layer in model.layers:
+   layer.trainable = True
+
+train_datagen = image.ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = image.ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        '../Mushrooms',
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+        '../Mushrooms',
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='binary')
+
+from keras.optimizers import SGD
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='sparse_categorical_crossentropy')
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800,
+        verbose=2)
+                
+model.save_weights('first_model.h5') 
