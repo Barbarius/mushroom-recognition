263b53bbedf71b36910108218119fad3b9f60d9e
Aliaksei <alexx333by@protonmail.com>
2019-03-26 10:51:32 +0300

    Added model saving in export directory

--- Model.py
+++ Model.py
@@ -51,4 +51,5 @@ model.fit_generator(
         verbose=2)
 
 export_dir = 'export/'
-model.save_weights(export_dir + 'first_model.h5') 
+model.save_weights(export_dir + 'weights.h5')
+np.savetxt(export_dir + 'model.json', [model.to_json()], fmt='%s')
