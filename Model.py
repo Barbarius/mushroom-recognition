7ef4e96ec695fa726223ed7e45a8a9720cda1323
Aliaksei <alexx333by@protonmail.com>
2019-03-31 19:26:24 +0300

    Splitted all the code into functions

--- Model.py
+++ Model.py
@@ -1,6 +1,7 @@
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.optimizers import SGD
 import tensorflow as tf
 import numpy as np
 import sys
@@ -18,49 +19,48 @@ if len(sys.argv) < 3:
 train_dir = sys.argv[1]
 validate_dir = sys.argv[2]
 
+def create_trainable_model(classes_count):
+    model = ResNet50(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=classes_count)
+    for layer in model.layers:
+        layer.trainable = True
+    return model
+
+def compile_model(model):
+    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='sparse_categorical_crossentropy')
+    return
+
+def train_model(model, train_dir, validation_dir, epochs):
+    train_datagen = image.ImageDataGenerator(
+            rescale=1./255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+    
+    test_datagen = image.ImageDataGenerator(rescale=1./255)
+    
+    train_generator = train_datagen.flow_from_directory(
+            train_dir,
+            target_size=(224, 224),
+            batch_size=32,
+            class_mode='binary')
+    
+    validation_generator = test_datagen.flow_from_directory(
+            validate_dir,
+            target_size=(224, 224),
+            batch_size=32,
+            class_mode='binary')
+    
+    model.fit_generator(
+            train_generator,
+            steps_per_epoch=2000,
+            epochs=epochs,
+            validation_data=validation_generator,
+            validation_steps=800,
+            verbose=2)
+    return
+    
 # ResNet model for classifications mushrooms into 4 categories:
 # edible, non-edible, partial-edible, not-a-mushroom
-model = ResNet50(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=4)
-
-for layer in model.layers:
-   layer.trainable = True
-
-train_datagen = image.ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
-
-test_datagen = image.ImageDataGenerator(rescale=1./255)
-
-train_generator = train_datagen.flow_from_directory(
-        train_dir,
-        target_size=(224, 224),
-        batch_size=32,
-        class_mode='binary')
-
-validation_generator = test_datagen.flow_from_directory(
-        validate_dir,
-        target_size=(224, 224),
-        batch_size=32,
-        class_mode='binary')
-
-from keras.optimizers import SGD
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='sparse_categorical_crossentropy')
-
-model.fit_generator(
-        train_generator,
-        steps_per_epoch=2000,
-        epochs=50,
-        validation_data=validation_generator,
-        validation_steps=800,
-        verbose=2)
-
-export_dir = 'export'
-keras_file = 'keras_model_and_weights.h5'
-tflite_file = os.path.join(export_dir, 'tflite_model_and_weigths.tflite')
-model.save(keras_file)
-
-converter = tf.lite.TFLiteConverter.from_keras_model_file(keras_file)
-tflite_model = converter.convert()
-open(tflite_file, "wb").write(tflite_model)
+model = create_trainable_model(4)
+compile_model(model)
+train_model(model, train_dir, validate_dir, 50)
