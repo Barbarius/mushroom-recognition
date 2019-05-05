b61349a638cfbb51ef85bb04ed1ab8c23eb0c8e5
Barbarius <s.bysov@mail.ru>
2019-05-05 19:12:17 +0300

    Train ResNet on Mushrooms_with_classes, 4 output classes, 20 epochs

--- Model.py
+++ Model.py
@@ -1,3 +1,7 @@
+#Retrain ResNet50 neural network model based on imagenet wheights
+
+#python3 Model.py 4 20 "../mushroom-images/Mushrooms_with_classes/" "../mushroom-images/Mushrooms_with_classes/"
+
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.optimizers import SGD
@@ -9,77 +13,58 @@ from keras.models import load_model
 from keras.models import Model
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras import metrics
+from keras.callbacks import CSVLogger
+from matplotlib import pyplot
 import tensorflow as tf
 import numpy as np
 import sys
 import os
 
 # Arguments:
-# 1 - programm name
-# 2 - directory with train fotos
-# 3 - directory with test fotos
-if len(sys.argv) < 3:
-    print("You should specify directory to train on as first argument and "
-        "directory to validate on as second one")
+# 0 - programm name
+# 1 - number of classes
+# 2 - number of epohs
+# 3 - directory with train fotos
+# 4 - directory with test fotos
+if len(sys.argv) < 5:
+    print("You should specify number of output classes as first argument, number of training epochs as second argument, "
+    "directory to train on as third argument and directory to validate on as forth argument.")
     exit()
 
-train_dir = sys.argv[1]
-validate_dir = sys.argv[2]
-
-'''
-def create_trainable_resnet50(classes_count):
-    model = ResNet50(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=classes_count)
-    for layer in model.layers:
-        layer.trainable = True
-    return model
-'''
-
-'''
-def create_trainable_resnet50(classes_count):
-	model = ResNet50(include_top=False, weights=None, input_shape=(224, 224, 3))
-
-	x = Flatten()(model.output)
-	predictions = Dense(classes_count, activation='softmax', name='fc1000')(x)
-	
-	model = Model(input=model.input, output=predictions)
-	for layer in model.layers:
-		layer.trainable = True
-	return model
-'''
+number_of_classes = int(sys.argv[1])
+number_of_epochs = int(sys.argv[2])
+train_dir = sys.argv[3]
+validate_dir = sys.argv[4]
 
 def create_trainable_resnet50(classes_count):
 	model = ResNet50(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
 
+    #do not retrain hole model
 	for layer in model.layers:
 		layer.trainable = False
 
+    #create new output layer and train it
 	x = Flatten()(model.output)
 	predictions = Dense(classes_count, activation='softmax', name='fc1000')(x)
 	
 	return Model(input=model.input, output=predictions)
-    
+
 def compile_model(model):
-    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='sparse_categorical_crossentropy')
-    #model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='sparse_categorical_crossentropy', metrics=['mse', 'mae', 'mape', 'cosine'])
     return
 
 def train_model(model, train_generator, validation_generator, epochs):
-    model.fit_generator(
+    csv_logger = CSVLogger('ResNet50_log.csv', append=True, separator=';')
+    history = model.fit_generator(
             train_generator,
             steps_per_epoch=2000,
             epochs=epochs,
             validation_data=validation_generator,
             validation_steps=800,
-            verbose=2)
-    return
-
-def save_model_to_pb(model, dir_path, file_name, output_node_name):
-    K.set_learning_phase(0)
-    tf.identity(model.output, name=output_node_name)
-    session = K.get_session()
-    constant_graph = graph_util.convert_variables_to_constants(session, session.graph.as_graph_def(), [output_node_name])
-    graph_io.write_graph(constant_graph, dir_path, file_name, as_text=False)
-    return
+            verbose=2,
+            callbacks=[csv_logger])
+    return history
 
 def get_data_generators(train_dir, validation_dir):
     train_datagen = image.ImageDataGenerator(
@@ -111,14 +96,22 @@ def save_class_labels(label_map, dir_path, file_name):
             label_file.write("%s\n" % label)
     return
 
+def save_model_to_pb(model, dir_path, file_name, output_node_name):
+    K.set_learning_phase(0)
+    tf.identity(model.output, name=output_node_name)
+    session = K.get_session()
+    constant_graph = graph_util.convert_variables_to_constants(session, session.graph.as_graph_def(), [output_node_name])
+    graph_io.write_graph(constant_graph, dir_path, file_name, as_text=False)
+    return
+    
 def save_model_to_h5(model, dir_path, file_name):
     model.save(os.path.join(dir_path, file_name))
     return
 
-model = create_trainable_resnet50(4)
+model = create_trainable_resnet50(number_of_classes)
 compile_model(model)
 train_generator, validation_generator = get_data_generators(train_dir, validate_dir)
-train_model(model, train_generator, validation_generator, 10)
+history = train_model(model, train_generator, validation_generator, number_of_epochs)
 compile_model(model)
 
 print(train_generator.class_indices)
--- export/model_with_weights.pb
+++ export/model_with_weights.pb
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7721c6f007311cce83122ed808b1da9fc8ecdd9cece13c2b2b468df9c2951d2c
+oid sha256:63aa922de65965336b9024767015fff7a99212769cc35662fe283b16b40a3167
 size 96141233
